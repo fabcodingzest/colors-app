f56cee3356d97598756ada513aa7e724ee0fccdc
fabcodingzest <fabrizvi786@gmail.com>
2019-11-12 20:17:06 +0530

    Display colors in SingleColorPalette

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
-
 import "./ColorBox.css";
-import { palette } from "@material-ui/system";
 
 class ColorBox extends Component {
   constructor(props) {
@@ -19,7 +17,7 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background, moreUrl } = this.props;
+    const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -38,9 +36,11 @@ class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
-          <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-            <span className="see-more">More</span>
-          </Link>
+          {showLink && (
+            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+              <span className="see-more">More</span>
+            </Link>
+          )}
         </div>
       </CopyToClipboard>
     );

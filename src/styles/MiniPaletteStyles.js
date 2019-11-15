dc930eff5c18ba303479571759d518256493fb87
fabcodingzest <fabrizvi786@gmail.com>
2019-11-15 23:38:15 +0530

    Add DeleteIcon to MiniPalette

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -6,8 +6,9 @@ export default {
     border: "1px solid black",
     position: "relative",
     overflow: "hidden",
-    "&:hover": {
-      cursor: "pointer"
+    cursor: "pointer",
+    "&:hover svg": {
+      opacity: 1
     }
   },
   colors: {
@@ -38,5 +39,18 @@ export default {
     position: "relative",
     margin: "0 auto",
     marginBottom: "-4px"
+  },
+  delete: {},
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0px",
+    top: "0px",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0
   }
 };
